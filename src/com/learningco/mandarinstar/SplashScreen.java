63ef22fb348498bbe9c76f5ff50254350a68a034
Chandy Doss <cdoss-consultant@scholastic.com>
2012-06-11 23:41:24 -0400

    renamed mardarinstaractivity to splashscreen

---
+++ src/com/learningco/mandarinstar/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.learningco.mandarinstar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		Thread logoTimer = new Thread() {
+			public void run() {
+				try {
+					sleep(1500);
+					Intent anIntent = new Intent(
+							"com.learningco.mandarinstar.Main");
+					startActivity(anIntent);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					finish();
+				}
+			}
+		};
+		logoTimer.start();
+	}
+}
