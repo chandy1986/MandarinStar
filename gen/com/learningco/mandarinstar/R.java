13c6afaacc2cd4a6a4c4b03a8f4a7ee851354020
Chandy Doss <cdoss-consultant@scholastic.com>
2012-06-12 16:01:46 -0400

    Added popup menu and empty menu methods

--- gen/com/learningco/mandarinstar/R.java
+++ gen/com/learningco/mandarinstar/R.java
@@ -11,15 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash=0x7f020001;
+        public static final int ic_about=0x7f020000;
+        public static final int ic_help=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_reset=0x7f020003;
+        public static final int splash=0x7f020004;
     }
     public static final class id {
-        public static final int imageView1=0x7f050000;
+        public static final int about=0x7f060003;
+        public static final int help=0x7f060002;
+        public static final int imageView1=0x7f060000;
+        public static final int reset=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int splash=0x7f030001;
+    }
+    public static final class menu {
+        public static final int mainmenu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/learningco/mandarinstar/Main.java
+++ src/com/learningco/mandarinstar/Main.java
@@ -2,6 +2,9 @@ package com.learningco.mandarinstar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Main extends Activity {
 	/** Called when the activity is first created. */
@@ -10,4 +13,45 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
+	
+	// Inflating a menu resource
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			MenuInflater inflater = getMenuInflater();
+			inflater.inflate(R.menu.mainmenu, menu);
+			return true;
+		}
+
+		// Handles menu item selection
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			switch (item.getItemId()) {
+			case R.id.reset:
+				reset();
+				return true;
+			case R.id.help:
+				help();
+				return true;
+			case R.id.about:
+				about();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+			}
+		}
+
+		private void about() {
+		    // TODO Auto-generated method stub
+		    
+		}
+
+		private void help() {
+		    // TODO Auto-generated method stub
+		    
+		}
+
+		private void reset() {
+		    // TODO Auto-generated method stub
+		    
+		}
 }
